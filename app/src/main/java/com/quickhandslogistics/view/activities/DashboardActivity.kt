db683c9f68c7a2cb79b45d63ee8c312291641c46
amit-opensource <amitgupta0294@gmail.com>
2020-01-13 18:48:56 +0530

    feature: LumperDetailsActivity: created activity

--- app/src/main/java/com/quickhandslogistics/view/activities/DashboardActivity.kt
+++ app/src/main/java/com/quickhandslogistics/view/activities/DashboardActivity.kt
@@ -28,8 +28,7 @@ class DashboardActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_lumper, R.id.nav_schedule, R.id.nav_reports
--- app/src/main/java/com/quickhandslogistics/view/adapter/LumperAdapter.kt
+++ app/src/main/java/com/quickhandslogistics/view/adapter/LumperAdapter.kt
@@ -1,33 +1,37 @@
 package com.quickhandslogistics.view.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.*
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.quickhandslogistics.R
+import com.quickhandslogistics.view.activities.LumperDetailsActivity
 import kotlinx.android.synthetic.main.item_lumper_layout.view.*
 
-class LumperAdapter (val items : ArrayList<String>, val context: Context) : Adapter<ViewHolder>() {
+class LumperAdapter(val items: ArrayList<String>, val context: Context) : Adapter<ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = LayoutInflater.from(context)
-            .inflate(R.layout.item_lumper_layout, parent, false)
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.item_lumper_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-
         return 12
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.constraintRoot.setOnClickListener {
+            context.startActivity(Intent(context, LumperDetailsActivity::class.java))
+        }
     }
-
 }
 
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
- val lumperText = view.text_lumper
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var lumperText = view.text_lumper
+    var constraintRoot = view.constraint_root
 }
