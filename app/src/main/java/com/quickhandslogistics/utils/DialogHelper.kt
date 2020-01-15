8c30c5adfe5a4d0cba217ccdb295f991dd14ffe1
amit-opensource <amitgupta0294@gmail.com>
2020-01-15 18:42:40 +0530

    feature: added strings and colors

--- app/src/main/java/com/quickhandslogistics/utils/DialogHelper.kt
+++ app/src/main/java/com/quickhandslogistics/utils/DialogHelper.kt
@@ -13,8 +13,23 @@ class DialogHelper {
         fun showDialog(title: String, activity : Activity) {
             val dialog = Dialog(activity)
             dialog .requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
             dialog .setCancelable(false)
             dialog .setContentView(R.layout.layout_dialog)
+
+            var text = dialog.findViewById<TextView>(R.id.text_message)
+            var yes = dialog.findViewById<TextView>(R.id.text_yes)
+            var no = dialog.findViewById<TextView>(R.id.text_no)
+
+            text.text = title
+
+            yes.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            no.setOnClickListener {
+                dialog.dismiss()
+            }
 
             dialog .show()
         }
