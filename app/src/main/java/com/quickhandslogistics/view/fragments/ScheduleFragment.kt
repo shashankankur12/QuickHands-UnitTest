3cc4e53e2e84f46d4f24b0717d2ba3d709ed330d
amit-opensource <amitgupta0294@gmail.com>
2020-01-20 19:06:45 +0530

    feature: ScheduleFragment: created calender

--- app/src/main/java/com/quickhandslogistics/view/fragments/ScheduleFragment.kt
+++ app/src/main/java/com/quickhandslogistics/view/fragments/ScheduleFragment.kt
@@ -9,41 +9,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.fragment_schedule.*
 import com.alamkanak.weekview.WeekViewEvent
 import com.alamkanak.weekview.MonthLoader
 import com.alamkanak.weekview.WeekView
 import com.quickhandslogistics.R
+import com.quickhandslogistics.model.DatesModel
+import com.quickhandslogistics.model.ScheduledEvents
+import com.quickhandslogistics.utils.Utils
 import com.quickhandslogistics.view.activities.AssignLumpersActivity
+import com.quickhandslogistics.view.adapter.DatesAdapter
+import com.quickhandslogistics.view.adapter.EventsAdapter
+import java.text.DateFormatSymbols
 import java.time.Month
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ScheduleFragment : Fragment(), MonthLoader.MonthChangeListener, WeekView.EventClickListener {
+class ScheduleFragment : Fragment(){
 
-    override fun onMonthChange(newYear: Int, newMonth: Int): MutableList<out WeekViewEvent> {
-        var list : MutableList<WeekViewEvent> = ArrayList< WeekViewEvent>(10)
+    private lateinit var datesAdapter : DatesAdapter
+    public lateinit var eventsList : ArrayList<ScheduledEvents>
+    private var numberOfDays: Int = 0
+    lateinit var datesList: java.util.ArrayList<DatesModel>
 
-        if (newMonth == 1) {
-            val startTime = Calendar.getInstance()
-            startTime.set(Calendar.HOUR_OF_DAY, 3)
-            startTime.set(Calendar.MINUTE, 0)
-            startTime.set(Calendar.MONTH, newMonth - 1)
-            startTime.set(Calendar.YEAR, newYear)
-
-            val endTime = startTime.clone() as Calendar
-            endTime.add(Calendar.HOUR, 2)
-            endTime.set(Calendar.MONTH, newMonth - 1)
-
-            val event = WeekViewEvent(1, "Location :  FlipKart, Gurgaon\nLumpers Required : 5", startTime, endTime)
-            event.color = resources.getColor(R.color.colorBlue)
-
-            list.add(event)
-        }
-
-        return list
-    }
+    var year: Int = 0
+    var month: Int = 0
+    var day: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,12 +50,74 @@ class ScheduleFragment : Fragment(), MonthLoader.MonthChangeListener, WeekView.E
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        week_view.monthChangeListener = this
-        week_view.setOnEventClickListener(this)
+        eventsList = ArrayList()
+        numberOfDays = getNumberOfDays()
+        prepareDatesList(numberOfDays, eventsList)
+
+        getCurrentMonthAndYear()
+       // setUpEventsRecyclerView(recycler_events, eventsList)
     }
 
-    override fun onEventClick(event: WeekViewEvent?, eventRect: RectF?) {
-        startActivity(Intent(this.requireActivity(), AssignLumpersActivity::class.java))
-        this.activity?.overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out)
+    private fun setUpDatesRecyclerView(recyclerView: RecyclerView) {
+        val mLinearLayoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.setLayoutManager(mLinearLayoutManager)
+
+        datesAdapter = DatesAdapter(this, datesList, this.requireActivity(), month, year, eventsList)
+        recyclerView.adapter = datesAdapter
+        recyclerView.scheduleLayoutAnimation()
     }
+
+    fun setUpEventsRecyclerView(recyclerView: RecyclerView, eventsList: java.util.ArrayList<ScheduledEvents>) {
+
+        val mLinearLayoutManager = LinearLayoutManager(activity)
+        mLinearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.setLayoutManager(mLinearLayoutManager)
+
+        val eventsAdapter = EventsAdapter(activity, eventsList, this)
+        recyclerView.setAdapter(eventsAdapter)
+        recyclerView.scheduleLayoutAnimation()
+    }
+
+
+    private fun getCurrentMonthAndYear() {
+        val mycal = Calendar.getInstance()
+
+        day = mycal.get(Calendar.DAY_OF_MONTH)
+        year = mycal.get(Calendar.YEAR)
+        month = mycal.get(Calendar.MONTH)
+
+        text_month_year.text = day.toString() + " " + getMonth(month) + " " + year
+    }
+
+    fun getMonth(month: Int): String {
+        return DateFormatSymbols().months[month]
+    }
+
+    private fun getNumberOfDays(): Int {
+        val mycal = Calendar.getInstance()
+        mycal.set(Calendar.YEAR, year)
+        mycal.set(Calendar.MONTH, month)
+        return mycal.getActualMaximum(Calendar.DAY_OF_MONTH)
+    }
+
+    private fun prepareDatesList(num: Int, events: java.util.ArrayList<ScheduledEvents>?) {
+
+        datesList = ArrayList()
+        datesList.clear()
+        for (i in 1..num) {
+
+            val date = DatesModel()
+            date.date = i
+            date.selected = false
+            date.eventAvailable = false
+
+            if (i == 2 || i ==7) date.eventAvailable = true
+
+            datesList.add(date)
+        }
+
+        setUpDatesRecyclerView(recycler_monthView)
+    }
+
 }
