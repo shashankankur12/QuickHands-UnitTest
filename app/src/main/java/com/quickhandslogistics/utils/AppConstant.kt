30fcc31cfa7564acd58b0f1c14b9eac1e9d0766a
Abhishek <abhishekkanojia493@gmail.com>
2020-01-20 19:13:55 +0530

    Feature: SettingsFragment: add localization

---
+++ app/src/main/java/com/quickhandslogistics/utils/AppConstant.kt
@@ -0,0 +1,9 @@
+package com.quickhandslogistics.utils
+
+interface AppConstant {
+
+    companion  object {
+        const val PREF_LANGUAGE_SELECT = "pref_language_select"
+        const val APP_SHARED_PREFERENCE = "app_shared_prefrences"
+    }
+}
