de31199bcbd20f763aff4a74eed33968a45db029
amit-opensource <amitgupta0294@gmail.com>
2020-01-29 18:21:10 +0530

    feature: EventsAdapter: implemented click event

--- app/src/main/java/com/quickhandslogistics/view/adapter/EventsAdapter.java
+++ app/src/main/java/com/quickhandslogistics/view/adapter/EventsAdapter.java
@@ -2,6 +2,7 @@ package com.quickhandslogistics.view.adapter;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.quickhandslogistics.R;
 import com.quickhandslogistics.model.ScheduledEvents;
+import com.quickhandslogistics.view.activities.AssignLumpersActivity;
 import com.quickhandslogistics.view.fragments.ScheduleFragment;
 
 import java.util.ArrayList;
@@ -42,9 +45,9 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
     @Override
     public void onBindViewHolder(@NonNull EventsHolder eventsHolder, int i) {
 
-        eventsHolder.textTitle.setText(Html.fromHtml("<b>Building : </b>One97 Communications Private Limited"));
+        eventsHolder.textTitle.setText(Html.fromHtml("<b>Building : One97 Communications Private Limited</b>"));
         eventsHolder.textCustomerName.setText(Html.fromHtml("<b>Door : </b>03"));
-        eventsHolder.textSubService.setText(Html.fromHtml("<b>Lumpers Required : </b>05"));
+        eventsHolder.textSubService.setText(Html.fromHtml("<b>Lumpers : </b>05"));
 
         if (i == 0) {
             eventsHolder.textTime.setText("09:00 AM");
@@ -53,6 +56,14 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
         } else {
             eventsHolder.textTime.setText("05:45 PM");
         }
+
+        eventsHolder.constraintRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActivity.startActivity(new Intent(mActivity, AssignLumpersActivity.class));
+                mActivity.overridePendingTransition(R.anim.anim_next_slide_in, R.anim.anim_next_slide_out);
+            }
+        });
     }
 
     @Override
@@ -66,6 +77,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
         TextView textCustomerName;
         TextView textSubService;
         TextView textTime;
+        ConstraintLayout constraintRoot;
 
         public EventsHolder(@NonNull View itemView) {
             super(itemView);
@@ -74,7 +86,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
             textCustomerName = itemView.findViewById(R.id.text_customer_name);
             textSubService = itemView.findViewById(R.id.text_service_subservice);
             textTime = itemView.findViewById(R.id.text_time);
+            constraintRoot = itemView.findViewById(R.id.constraint_root);
         }
-
     }
 }
