b9747f41f47e18faa6d0e71ef84c38b58bbc4c60
karishmabh <karishma.bhardwaj@talent4assure.in>
2020-03-23 20:30:04 +0530

    Feature : AssignedLumperAdapter : make ui changes

--- app/src/main/java/com/quickhandslogistics/view/adapter/AssignedLumperAdapter.kt
+++ app/src/main/java/com/quickhandslogistics/view/adapter/AssignedLumperAdapter.kt
@@ -2,10 +2,13 @@ package com.quickhandslogistics.view.adapter
 
 import android.app.Activity
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -35,12 +38,11 @@ class AssignedLumperAdapter(val context: Activity, private val canReplace: Boole
     override fun onBindViewHolder(holder: WorkItemHolder, position: Int) {
         holder.lumperText?.text = faker.name.firstName() + " " + faker.name.lastName()
 
-        Picasso.get().load(faker.avatar.image()).error(R.drawable.ic_basic_info_placeholder)
+        Picasso.get().load(R.drawable.ic_basic_info_placeholder).error(R.drawable.ic_basic_info_placeholder)
             .into(holder.profilePic)
 
         if (position == 1 || position == 3) {
-            holder.textStatus.setBackgroundResource(R.drawable.chip_complete)
-            holder.textStatus.text = "NO SHOW"
+            holder.viewAttendanceStatus.setBackgroundResource(R.drawable.offline_dot)
         }
 
         if (position == replacedPosition) {
@@ -62,26 +64,30 @@ class AssignedLumperAdapter(val context: Activity, private val canReplace: Boole
         holder.buttonReplace.setOnClickListener {
             showAssignLumperDialog(context, position)
         }
+
     }
 
     fun showAssignLumperDialog(activity: Activity, position: Int) {
 
         val dialogAsk = Dialog(activity)
 
-        dialogAsk.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialogAsk.window?.setBackgroundDrawableResource(android.R.color.transparent)
-        dialogAsk.window?.attributes?.windowAnimations = R.style.dialogAnimation
-        dialogAsk.setCancelable(false)
+        dialogAsk?.window?.attributes?.windowAnimations = R.style.dialogAnimation
+        dialogAsk?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialogAsk?.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialogAsk?.setCancelable(false)
         dialogAsk.setContentView(R.layout.dialog_assign_lumper_confirmation)
 
         val window = dialogAsk.window
         window?.setLayout(
-            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
 
-        val textYes = dialogAsk.findViewById<TextView>(R.id.text_yes)
-        val textNo = dialogAsk.findViewById<TextView>(R.id.text_no)
+        val textYes = dialogAsk.findViewById<Button>(R.id.text_yes)
+        val textNo = dialogAsk.findViewById<Button>(R.id.text_no)
 
         textYes.setOnClickListener {
             replacedPosition = position
@@ -95,61 +101,15 @@ class AssignedLumperAdapter(val context: Activity, private val canReplace: Boole
 
         dialogAsk.show()
     }
-}
 
+}
 
 class WorkItemHolder(view: View) : RecyclerView.ViewHolder(view) {
-    var lumperText = view.text_lumper
-    var textStatus = view.text_status
-    var profilePic = view.image_lumper_logo
+    var lumperText = view.textViewLumperName
+    var viewAttendanceStatus: View = view.viewAttendanceStatus
+    var profilePic = view.circleImageViewProfile
     var buttonReplace = view.button_replace
     var text_replaced = view.text_replaced
-    var constraintRoot = view.constraint_root
-}
 
-//fun showDialog(name: String, activity: Activity) {
-//    val dialog = Dialog(activity)
-//
-//    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-//    dialog.window?.attributes?.windowAnimations = R.style.dialogAnimation
-//    dialog.setCancelable(false)
-//    dialog.setContentView(R.layout.layout_dialog_lumper_attendence)
-//
-//    val window = dialog.getWindow()
-//    window?.setLayout(
-//        LinearLayout.LayoutParams.MATCH_PARENT,
-//        LinearLayout.LayoutParams.WRAP_CONTENT
-//    )
-//
-//    var textName = dialog.findViewById<TextView>(R.id.text_lumper_name)
-//    var radioAttendance = dialog.findViewById<RadioGroup>(R.id.radio_attendance)
-//  //  var comment = dialog.findViewById<EditText>(R.id.text_comment)
-//    var buttonSubmit = dialog.findViewById<TextView>(R.id.text_submit)
-//    var group = dialog.findViewById<Group>(R.id.group_comment)
-//
-//    textName.text = name
-//
-//    radioAttendance.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
-//        override fun onCheckedChanged(radioGroup: RadioGroup?, p1: Int) {
-//
-//            if (radioGroup?.checkedRadioButtonId == R.id.radio_no_show) {
-//                group.visibility = View.VISIBLE
-//            } else {
-//                group.visibility = View.GONE
-//            }
-//        }
-//    })
-//
-//    buttonSubmit.setOnClickListener {
-//
-//        dialog.dismiss()
-//
-//        if (radioAttendance?.checkedRadioButtonId == R.id.radio_no_show) {
-//            showAssignLumperDialog(activity)
-//        }
-//    }
-//
-//    dialog.show()
-//}
+}
 
