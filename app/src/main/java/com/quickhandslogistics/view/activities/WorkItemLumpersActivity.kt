f630f14609fcbb51ffb3ef0c631d26cc04064a55
karishmabh <karishma.bhardwaj@talent4assure.in>
2020-03-23 20:30:47 +0530

    Refactor : dialog_assign_lumper_confirmation.xml

--- app/src/main/java/com/quickhandslogistics/view/activities/WorkItemLumpersActivity.kt
+++ app/src/main/java/com/quickhandslogistics/view/activities/WorkItemLumpersActivity.kt
@@ -1,15 +1,14 @@
 package com.quickhandslogistics.view.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.quickhandslogistics.R
+import com.quickhandslogistics.modified.views.BaseActivity
 import com.quickhandslogistics.view.adapter.AssignedLumperAdapter
 import kotlinx.android.synthetic.main.activity_work_item_lumpers.*
-import kotlinx.android.synthetic.main.layout_header.*
 
-
-class WorkItemLumpersActivity : AppCompatActivity() {
+class WorkItemLumpersActivity : BaseActivity() {
 
     companion object {
         const val ARG_CAN_REPLACE = "ARG_CAN_REPLACE"
@@ -18,20 +17,20 @@ class WorkItemLumpersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_item_lumpers)
-
-        text_title.text = getString(R.string.string_lumper)
-
-        image_back.setOnClickListener {
-            finish()
-            overridePendingTransition(R.anim.anim_prev_slide_in, R.anim.anim_prev_slide_out)
-        }
+        setupToolbar("Container : ICNU7869687")
 
         intent.extras?.let { it ->
             if (it.containsKey(ARG_CAN_REPLACE)) {
                 val canReplace = it.getBoolean(ARG_CAN_REPLACE, true)
 
-                recycler_lumpers.layoutManager = LinearLayoutManager(this)
-                recycler_lumpers.adapter = AssignedLumperAdapter(this, canReplace)
+                recycler_lumpers.apply {
+                    val linearLayoutManager = LinearLayoutManager(this@WorkItemLumpersActivity)
+                    layoutManager = linearLayoutManager
+                    val dividerItemDecoration =
+                        DividerItemDecoration(this@WorkItemLumpersActivity, linearLayoutManager.orientation)
+                    addItemDecoration(dividerItemDecoration)
+                    adapter = AssignedLumperAdapter(this@WorkItemLumpersActivity, canReplace)
+                }
             }
         }
     }
