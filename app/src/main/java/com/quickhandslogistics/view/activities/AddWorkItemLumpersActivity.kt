71eab657eb2467b3efb5fcda548298e832597e91
karishmabh <karishma.bhardwaj@talent4assure.in>
2020-03-25 14:59:48 +0530

    Feature : AddWorkItemLumpersActivity : apply search functionality on lumper list

--- app/src/main/java/com/quickhandslogistics/view/activities/AddWorkItemLumpersActivity.kt
+++ app/src/main/java/com/quickhandslogistics/view/activities/AddWorkItemLumpersActivity.kt
@@ -3,19 +3,23 @@ package com.quickhandslogistics.view.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.quickhandslogistics.R
+import com.quickhandslogistics.modified.data.lumperSheet.LumperModel
 import com.quickhandslogistics.modified.views.BaseActivity
+import com.quickhandslogistics.utils.Utils
 import com.quickhandslogistics.view.adapter.AddLumperAdapter
 import kotlinx.android.synthetic.main.activity_add_work_item_lumpers.*
 
-class AddWorkItemLumpersActivity : BaseActivity(),View.OnClickListener {
+class AddWorkItemLumpersActivity : BaseActivity(),View.OnClickListener, TextWatcher {
 
     private var position: Int = 0
     private lateinit var addLumperAdapter: AddLumperAdapter
-    private var addedLumpers: ArrayList<String> = ArrayList()
+    private var addedLumpers: ArrayList<LumperModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +39,13 @@ class AddWorkItemLumpersActivity : BaseActivity(),View.OnClickListener {
         }
 
         button_submit.setOnClickListener(this)
+        editTextSearch.addTextChangedListener(this)
+        imageViewCancel.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
-        view?.let{
-            when(view.id){
+        view?.let {
+            when (view.id) {
                 button_submit.id -> {
                     addedLumpers = addLumperAdapter.getSelectedLumper()
                     if (addedLumpers.size > 0) {
@@ -50,7 +56,23 @@ class AddWorkItemLumpersActivity : BaseActivity(),View.OnClickListener {
                         finish()
                     }
                 }
+
+                imageViewCancel.id -> {
+                    editTextSearch.setText("")
+                    Utils.hideSoftKeyboard(activity)
+                }
             }
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {}
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        text?.let {
+            addLumperAdapter.setSearchEnabled(text.isNotEmpty(), text.toString())
+            imageViewCancel.visibility = if (text.isNotEmpty()) View.VISIBLE else View.GONE
         }
     }
 
--- app/src/main/java/com/quickhandslogistics/view/adapter/AddLumperAdapter.kt
+++ app/src/main/java/com/quickhandslogistics/view/adapter/AddLumperAdapter.kt
@@ -7,22 +7,26 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.quickhandslogistics.R
+import com.quickhandslogistics.modified.data.lumperSheet.LumperModel
 import com.squareup.picasso.Picasso
 import io.bloco.faker.Faker
 import kotlinx.android.synthetic.main.layout_add_lumpers.view.*
-
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AddLumperAdapter(val context: Activity) :
     Adapter<AddLumperAdapter.WorkItemHolder>() {
-
-    var addedLumpersList: ArrayList<String> = ArrayList()
-    var lumpers: ArrayList<String> = ArrayList()
+    var addedLumpersList: ArrayList<LumperModel> = ArrayList()
+    var lumpers: ArrayList<LumperModel> = ArrayList()
+    var lumpersFilter: ArrayList<LumperModel> = ArrayList()
+    private var searchEnabled = false
+    private var searchTerm = ""
 
     init {
-        lumpers.add("Gene Hand")
-        lumpers.add("Frida Moore")
-        lumpers.add("Virgil Ernser")
-        lumpers.add("Philip Von")
+        lumpers.add(LumperModel("Gene ","Hand","ffr"))
+        lumpers.add(LumperModel("Frida", "Moore",""))
+        lumpers.add(LumperModel("Virgil", "Ernser", ""))
+        lumpers.add(LumperModel("Philip", "Von", ""))
     }
 
     var faker = Faker()
@@ -33,14 +37,16 @@ class AddLumperAdapter(val context: Activity) :
     }
 
     override fun getItemCount(): Int {
-        return 4
+        return if (searchEnabled) lumpersFilter.size else lumpers.size
+    }
+
+    private fun getItem(position: Int): LumperModel {
+        return if (searchEnabled) lumpersFilter[position] else lumpers[position]
     }
 
     override fun onBindViewHolder(holder: WorkItemHolder, position: Int) {
-        holder.lumperText?.text = lumpers[position]
 
-        Picasso.get().load(R.drawable.ic_basic_info_placeholder).error(R.drawable.ic_basic_info_placeholder)
-            .into(holder.profilePic)
+        holder.bind(getItem(position))
 
         if (addedLumpersList.contains(lumpers[position])) {
             holder.imageViewAdd.setImageResource(R.drawable.ic_add_lumer_tick)
@@ -57,15 +63,63 @@ class AddLumperAdapter(val context: Activity) :
             }
             notifyDataSetChanged()
         }
+
     }
 
-    fun getSelectedLumper(): ArrayList<String> {
-        return addedLumpersList
+    fun getSelectedLumper(): ArrayList<LumperModel> {
+        return lumpers
+    }
+
+    fun updateLumpersData(lumperDataList: ArrayList<LumperModel>) {
+        lumpers.clear()
+        lumpers.addAll(lumperDataList)
+        notifyDataSetChanged()
     }
 
     class WorkItemHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var lumperText = view.textViewLumperName
-        var profilePic = view.circleImageViewProfile
+        var textViewLumperName = view.textViewLumperName
+        var imageProfile = view.circleImageViewProfile
         var imageViewAdd = view.viewAttendanceStatus
+
+        fun bind(lumperModelData: LumperModel) {
+            textViewLumperName.text = String.format(
+                "%s %s",
+                lumperModelData.name.toUpperCase(Locale.getDefault()),
+                lumperModelData.lastName.toUpperCase(Locale.getDefault())
+            )
+
+            Picasso.get().load(R.drawable.ic_basic_info_placeholder)
+                .error(R.drawable.ic_basic_info_placeholder)
+                .into(imageProfile)
+
+        }
+        }
+
+    fun setSearchEnabled(enabled: Boolean, searchTerm: String = "") {
+        this.searchEnabled = enabled
+        if (!searchEnabled) {
+            this.searchTerm = ""
+            lumpersFilter.clear()
+            notifyDataSetChanged()
+            return
+        }
+        this.searchTerm = searchTerm.toLowerCase(Locale.getDefault())
+        filter()
     }
-}
+
+    private fun filter() {
+        lumpersFilter.clear()
+        if (searchTerm.isEmpty()) {
+            lumpersFilter.addAll(lumpers)
+        } else {
+            for (data in lumpers) {
+                val term = "${data.name} ${data.lastName}"
+
+                if (term.toLowerCase(Locale.getDefault()).contains(searchTerm)) {
+                    lumpersFilter.add(data)
+                }
+            }
+        }
+        notifyDataSetChanged()
+    }
+    }
